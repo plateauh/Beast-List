e99adb0b96f9b9f1ffb29a9b99cd2e880569f080
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-25 22:44:24 +0300

    Done

--- NajedBeastList/ViewController.swift
+++ NajedBeastList/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Create this array at the top of your class as a property
+    var tasks = ["Exercise for 30 minutes", "Wireframe for some project", "Do laundry"]
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if !(textField.text?.isEmpty ?? false) {
+            tasks.append(textField.text ?? "None")
+            tableView.reloadData()
+            textField.text = ""
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
+}
+
+extension ViewController: UITableViewDataSource {
+
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
 
 }
 
